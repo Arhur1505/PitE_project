40053da70b2b8d9caf5857b1854eedfbe10801b2
Artur <rutra2003@onet.pl>
2025-01-24 19:49:05 +0100

    better agent

---
+++ dqn_train.py
@@ -0,0 +1,32 @@
+from stable_baselines3 import DQN
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import DummyVecEnv
+from modules.hill_climb_env import HillClimbEnv
+
+env = Monitor(HillClimbEnv(max_steps=1000, debug=False))
+vec_env = DummyVecEnv([lambda: env])
+
+model = DQN(
+    "MlpPolicy",
+    vec_env,
+    learning_rate=1e-4,
+    buffer_size=50000,
+    learning_starts=1000,
+    batch_size=32,
+    target_update_interval=500,
+    train_freq=4,
+    gamma=0.99,
+    verbose=1,
+    tensorboard_log="./dqn_hill_climb/",
+)
+
+model.learn(total_timesteps=100000, log_interval=10)
+model.save(".models/dqn_hill_climb")
+
+obs, _ = env.reset()
+for _ in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, truncated, _ = env.step(action)
+    env.render()
+    if done or truncated:
+        obs, _ = env.reset()
